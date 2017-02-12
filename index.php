2c44158a39f68fdcd8a1a40a0c9681c92a4553ae
MJFoster <maryjofoster@u.boisestate.edu>
2017-02-11 22:54:20 -0700

    Initial commit from from desktop c:\wamp64\www\InvestAndLearn.net

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 	include_once 'index.html';
-	$value = 25%5;
-	echo "<br><br>"."Hello from php! Again!!!";
+	$value = 25/3;
+	echo "<br><br>"."Hello from php!"."<br>"."I'm in InvestAndLearn.net!!!";
 	echo "<br>".$value;
 ?>
