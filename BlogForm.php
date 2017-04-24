daa14d6eecfc08078fa29ec1b996ef8105d7a428
MJFoster <maryjofoster@u.boisestate.edu>
2017-04-24 01:30:10 -0600

    - Added <textarea> to blog form. - Added content to home page. - Updated more styles.

--- BlogForm.php
+++ BlogForm.php
@@ -1,20 +1,13 @@
 <?php
+    $thisPage = "Blog-Page";
     include("Header.php");
 ?>
     <div>
         <form class="add-form" id="blog-form" method="POST" action="BlogHandler.php">
             <div class="form-inputs">
 
-                <label for="postText" class="required">* Post Text: </label>
-                <input id="post-text" type="text"
-                    placeholder="Enter your post ..."
-                    <?php
-                        if (isset($_SESSION['postText'])) {
-                            echo 'value="' . $_SESSION['postText'] . '"';
-                        }
-                    ?>
-                    name="postText" required>
-
+                <div for="postText" class="required">* Enter Blog Entry Below:</div>
+                <textarea id="post-text" name="postText" required cols="75" rows="10"></textarea>
                 <input id="add-form-submit" type="submit">
             </div>
         </form>
--- index.php
+++ index.php
@@ -5,25 +5,26 @@
     include("Header.php");
 ?>
 
-<div class="main-content black-text simple-border">
+<div class="main-content black-text simple-border white-background">
     <img id="home-page-image" src="Images/blue-$$$.jpg" alt="Stacked Money"/>
 	<div>
 		<div id="home-page-content" class="dark-purple-text">
 			<p>
-				TBD ... A paragraph will go here that's aim is to motivate
-				visitors to learn more about saving, growing, and investing money purposefully.
+				Interested in putting your money to work so you don't have to when you finally retire?
 			</p>
 			<p>
-				TBD ... A paragraph will go here that's aim is to motivate
-				visitors to learn more about saving, growing, and investing money purposefully.
+				Are you wondering where to turn for advise, and worried about being taken advantage
+				of?
 			</p>
 			<p>
-				TBD ... A paragraph will go here that's aim is to motivate
-				visitors to learn more about saving, growing, and investing money purposefully.
+				Join us here, and learn to invest at your own pace with as little money as you'd like.
 			</p>
 			<p>
-				TBD ... A paragraph will go here that's aim is to motivate
-				visitors to learn more about saving, growing, and investing money purposefully.
+				We want to grow our money also, so we lower the risks by investing together and learning 
+				along the way.  Learning is key, so come along and we'll show you how!
+			</p>
+			<p>
+				We like to grow our money also, but learning how to do it smart is really our priority!
 			</p>
 		</div>
 		<button id="testimonials-button" class="buttons"><a href="TestimonialsPage.php">Testimonials</a></button>
